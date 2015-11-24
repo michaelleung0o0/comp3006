78bbfe8bdb0e82fc90e091ecd04d59bcda70fbe6
kittenmak <kittenmak@gmail.com>
2015-11-24 14:44:19 +0800

    enhance of monitoring sql return

--- monitoring.py
+++ monitoring.py
@@ -16,11 +16,13 @@ class monitoring():
         findUIDSQL = "select uid from userinfo where username = '" + user +"'"
         uidL = self.sql.runSQL(findUIDSQL).fetchall()[0]
         uid = uidL[0]
-        findMList = "select * from monitoring where uid = '" + str(uid) +"'"
+        findMList = "select s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20 from monitoring where uid = '" + str(uid) +"'"
         MListResult = self.sql.runSQL(findMList).fetchall()
-        return MListResult
+        return MListResult[0]
 
 
 m = monitoring(00001, 5)
-print(m.requestMList())
+#print(m.requestMList())
+var = m.requestMList()
+print(var)[0]
                 
