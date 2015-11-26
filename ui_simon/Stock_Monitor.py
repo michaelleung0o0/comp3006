55e83c01f581843b0a9dc574176136317261b7da
simonyeung1995 <yeungmankit@YeungMandeMacBook-Pro.local>
2015-11-26 16:52:30 +0800

    With about us

--- ui_simon/Stock_Monitor.py
+++ ui_simon/Stock_Monitor.py
@@ -269,7 +269,8 @@ class Login ( wx.Panel ):
 		self.Setting.Show()
 
 	def onAboutUs( self, event ):
-		event.Skip()
+		self.About_Us = About_Us(self)
+		self.About_Us.Show()
 	
 	def onNews( self, event ):
 		self.Hide()
@@ -406,7 +407,8 @@ class Welcome ( wx.Panel ):
 		self.Setting.Show()
 	
 	def onAbout( self, event ):
-		event.Skip()
+		self.About_Us = About_Us(self)
+		self.About_Us.Show()
 	
 	def onAdmin( self, event ):
 		self.Hide()
@@ -546,7 +548,8 @@ class Finances ( wx.Panel ):
 		self.Setting.Show()
 	
 	def onAbout( self, event ):
-		event.Skip()
+		self.About_Us = About_Us(self)
+		self.About_Us.Show()
 	
 	def onAdmin( self, event ):
 		self.Hide()
@@ -715,7 +718,8 @@ class Search_Stock ( wx.Panel ):
 		self.Setting.Show()
 	
 	def onAbout( self, event ):
-		event.Skip()
+		self.About_Us = About_Us(self)
+		self.About_Us.Show()
 	
 	def onAdmin( self, event ):
 		self.Hide()
@@ -864,7 +868,8 @@ class Monitoring_stock ( wx.Panel ):
 		self.Setting.Show()
 	
 	def onAbout( self, event ):
-		event.Skip()
+		self.About_Us = About_Us(self)
+		self.About_Us.Show()
 	
 	def onAdmin( self, event ):
 		self.Hide()
@@ -1056,7 +1061,8 @@ class Admin ( wx.Panel ):
 		self.Setting.Show()
 	
 	def onAbout( self, event ):
-		event.Skip()
+		self.About_Us = About_Us(self)
+		self.About_Us.Show()
 	
 	def onAdmin( self, event ):
 		self.Hide()
@@ -1454,6 +1460,57 @@ class Setting ( wx.Frame ):
 	def onCancel( self, event ):
 		self.Destroy()
 	
+aboutUSText = "This system is developed by 404 NOT FOUND team from Hong Kong Baptist University.\n 404 NOT FOUND:\n13207296 YEUNG MAN KIT\n13205633 CHAN WING SUN\n12210102 MAK KIT TIN\n15208249 LEUNG KAI HIN"
+
+class About_Us ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"About Us", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer62 = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_panel11 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer63 = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_staticText30 = wx.StaticText( self.m_panel11, wx.ID_ANY, u"About Us", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText30.Wrap( -1 )
+		self.m_staticText30.SetFont( wx.Font( 20, 70, 90, 92, False, wx.EmptyString ) )
+
+		bSizer63.Add( self.m_staticText30, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+		self.m_richText3 = wx.richtext.RichTextCtrl( self.m_panel11, wx.ID_ANY, aboutUSText, wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY|wx.VSCROLL|wx.HSCROLL|wx.NO_BORDER|wx.WANTS_CHARS )
+		bSizer63.Add( self.m_richText3, 1, wx.EXPAND |wx.ALL, 5 )
+
+		self.exit = wx.Button( self.m_panel11, wx.ID_ANY, u"&Exit", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer63.Add( self.exit, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+
+		self.m_panel11.SetSizer( bSizer63 )
+		self.m_panel11.Layout()
+		bSizer63.Fit( self.m_panel11 )
+		bSizer62.Add( self.m_panel11, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		self.SetSizer( bSizer62 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.exit.Bind( wx.EVT_BUTTON, self.onExit )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def onExit( self, event ):
+		# event.Skip()
+		self.Destroy()
+
+
 class IndexPage ( wx.Frame ):
     def __init__( self, parent ):
 		wx.Frame.__init__( self, parent, wx.ID_ANY, size= wx.Size( 900,620 ))
