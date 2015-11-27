c680ae5329e39b32e1296f2557c58eb9d0dffdf5
michaelleung <michael.leung@mbounce.com>
2015-11-27 20:04:50 +0800

    changed: apiRequest + Search_Stock
    debug: github crash

--- ui_michael/apiRequest.py
+++ ui_michael/apiRequest.py
@@ -2,51 +2,68 @@ import requests
 import xml.etree.ElementTree as ET
 from codecs import EncodedFile
 
-# mUrl = "http://202.125.90.199/securityQuote/genStockXMLHK.php?stockcode=00001";
-mUrl = 'http://cn.wsj.com/big5/rssHKstock.xml'
-from_encoding = 'big5'
-to_encoding = 'utf-8'
+mUrl = "http://202.125.90.199/securityQuote/genStockXMLHK.php?stockcode=00001";
+# mUrl = 'http://cn.wsj.com/big5/rssHKstock.xml'
+# from_encoding = 'big5'
+# to_encoding = 'utf-8'
+
 
 class apiRequest():
-	def __init__(self, parent):
+   def __init__(self, parent):
 		# super(apiRequest, self).__init__()
-		self.GetStockRequest()
+      # english = None
+		# self.GetStockRequest()
 
-	def GetStockRequest(self):
-	    resp = requests.get(mUrl, stream=True)
-	    if 200 != resp.status_code:
-	        return False
+  #  def GetStockRequest(self):
+      resp = requests.get(mUrl, stream=True)
+      if 200 != resp.status_code:
+         return False
 	        # xml = resp.content.replace("big5","utf-8")
 	        # xml = resp.content.decode('big5')
 	        # data = xml.decode('utf-8')
             # root = ET.fromstring(data)
+      xml = resp.content.replace("utf8","utf-8")
+      self.root = ET.fromstring(xml)
+      # print root.tag
+      self.stock = self.root.find('stock')
+      self.server = self.stock.find('server')
+      self.name = self.stock.find('name')
+      # self.chinese = self.name.find('chinese').text
+      #self.schinese = self.name.find('schinese').text
+      self.english = self.name.find('english').text
+      self.price = self.stock.find('price').text
+      self.pexit = self.stock.find('pexit').text
+      self.highPrice = self.stock.find('high').text
+      self.lowPrice = self.stock.find('low').text
+      self.change = self.stock.find('change').text
+      self.pctChange = self.stock.find('pct_change').text
 
-   #          xml = resp.content.replace("utf8", "utf-8")
-   #          root = ET.fromstring(xml)
-   #          print root.tag
-   #          stock = root.find('stock')
-   #          #
-   #          server = stock.find('server')
-			# #
-   #          name = stock.find('name')
-   #          chinese = name.find('chinese')
-   #          schinese = name.find('schinese')
-   #          english = name.find('english')
-   #          #
-   #          price = stock.find('price')
-   #          print english.text , price.text
-			# xml = resp.content.decode('utf-8')
+   # def GetChineseName(self, event):
+   #    return self.chinese
 
-   #          stock = root.find('stock')
-   #          #
-   #          server = stock.find('server')
-			# #
-   #          name = stock.find('name')
-   #          chinese = name.find('chinese')
-   #          schinese = name.find('schinese')
-   #          english = name.find('english')
-   #          #
-   #          price = stock.find('price')
-            # print english.text , price.text
-            data = EncodedFile(resp.content, from_encoding, to_encoding)
-				print data
+   # def GetSchineseName(self, event):
+   #    return self.schinese
+
+   def GetEnglishName(self, event):
+      return self.english
+
+   def GetCurrentPrice(self, event):
+      return self.price
+
+   def GetPrefixPrice(self, event):
+      return self.pexit
+
+   def GetHighPrice(self, event):
+      return self.highPrice
+
+   def GetLowPrice(self, event):
+      return self.lowPrice
+
+   def GetChange(self, event):
+      return self.change
+
+   def GetPctChange(self, event):
+      return self.pctChange
+   def UpdateSearchCode(self,event, symbol):
+      mUrl = 'http://202.125.90.199/securityQuote/genStockXMLHK.php?stockcode=' + symbol
+      print mUrl
